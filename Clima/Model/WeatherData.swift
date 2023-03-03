ddffbdffd97165eadc6ce28e5c9d723ce81b82b9
Nurgali Nuraly <nurali.nurgali@gmail.com>
2023-03-03 17:45:46 +0600

    Setup the app

---
+++ Clima/Model/WeatherData.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Nurgali on 03.03.2023.
+//
+
+import Foundation
+
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+}
+
+struct Weather: Codable {
+    let description: String
+    let id: Int
+}
